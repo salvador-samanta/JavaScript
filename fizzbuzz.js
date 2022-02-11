2e30e6abf3157336c54f9f8fa65fd9bb43d133f9
salvador-samanta <99303558+salvador-samanta@users.noreply.github.com>
2022-02-11 00:06:27 -0300

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,7 @@
+var x, n 
+for (var x; x < 101; i++){
+    if (x % 15 == 0) console.log("Fizz Buzz");
+    else if (x % 3 == 0) console.log("Fizz");
+    else if (x % 5 == 0) console.log("Buzz");
+    else console.log(x);
+}
